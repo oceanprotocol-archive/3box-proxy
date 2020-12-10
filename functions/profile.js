b173759ed989086b838cbed3d4b12d5a171b4b4b
mihaisc <mihai.scarlat@smartcontrol.ro>
2020-12-10 15:00:11 +0200

    fix queryString
    Signed-off-by: mihaisc <mihai.scarlat@smartcontrol.ro>

--- functions/profile.js
+++ functions/profile.js
@@ -1,27 +1,30 @@
 const fetch = require("node-fetch");
 
 const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET'
-  };
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET",
+};
 exports.handler = async (event) => {
   const apiUri = "https://ipfs.3box.io";
 
   try {
-    console.log(event)
+    console.log(event);
     const id = event.queryStringParameters.series
-    ? event.queryStringParameters.series
-    : event.path.split("/")[2];
-    console.log(id);
-    
-    const response = await fetch(`${apiUri}/profile?address=${event.queryStringParameters,event.queryStringParameters.id}`);
+      ? event.queryStringParameters.series
+      : event.path.split("/")[2];
+    console.log("id", id);
+
+    const response = await fetch(
+      `${apiUri}/profile?address=${
+        (event.queryStringParameters, event.queryStringParameters.id)
+      }`
+    );
 
     // upon 404, fail silently and return
     if (!response || !response.ok || response.status !== 200) {
       return {
         statusCode: 200,
-        headers,
         body: "error",
       };
     }
@@ -29,13 +32,12 @@ exports.handler = async (event) => {
     const responseJson = await response.json();
     return {
       statusCode: 200,
-      headers,
       body: JSON.stringify(responseJson),
     };
   } catch (error) {
+    console.error(error);
     return {
       statusCode: 200,
-      headers,
       body: "error",
     };
   }
